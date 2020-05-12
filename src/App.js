5021c745ceb2f4a5212a057b9fd7f82048b1241c
Adedeji Abiola <adedejiabiola56@gmail.com>
2020-05-12 23:00:51 +0100

    update app woth shop page, collection preview cont

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/Homepage';
 import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop-page/ShopPage';
 
-
-function HatsPage() {
-  return <h1>Hats Page Here</h1>
-}
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/:shop' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/shop-page/ShopPage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { SHOP_DATA } from '../../utils/_Data'
+import CollectionPreview from '../../components/collection-preview/CollectionPreview'
+
+export class ShopPage extends Component {
+  state = {
+    collections: SHOP_DATA
+  }
+
+  render() {
+    return (
+      <div className='shop-page'>
+        {
+          this.state.collections.map(({ id, ...restOfCollection}) => (
+            <CollectionPreview key={id} {...restOfCollection} />
+          ))
+        }
+      </div>
+    )
+  }
+}
+
+export default ShopPage
+
