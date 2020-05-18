40ce1a12cdab67f310ddf5f63a7939b781f6326d
Adedeji Abiola <adedejiabiola56@gmail.com>
2020-05-18 06:31:24 +0100

    convert shop data from array to an object

--- src/components/collection/Collection.js
+++ src/components/collection/Collection.js
@@ -4,14 +4,19 @@ import { connect } from 'react-redux'
 import { selectCollection } from '../../redux/shop/shop.selectors'
 import CollectionItem from '../collection-item/CollectionItem'
 
-const CollectionPage = ({ collection: {items} }) => {
+const CollectionPage = ({ collection }) => {
+  const { items, title } = collection
+  console.log(collection)
   return (
     <div className='collection-page'>
-      {
-        items.map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))
-      }
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {
+          items.map(item => (
+            <CollectionItem key={item.id} item={item} />
+          ))
+        }
+      </div>
     </div>
   )
 }
