b09f209048d33192f576fdf8ba5baa821f119923
Adedeji Abiola <adedejiabiola56@gmail.com>
2020-09-16 00:16:50 +0100

    register service workers and modify manifest.json for pwa functionality

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
+app.get('/service-worker.js', (req, res) => {
+  res.send(path.resolve(__dirname, '..', 'build', 'service-worker.js'))
+})
+
 app.post('/payment', (req, res) => {
   const body = {
     source: req.body.token.id,
