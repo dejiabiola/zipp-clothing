a1a8e813ac73e37f59f02c93644986c6a34349b4
Adedeji Abiola <adedejiabiola56@gmail.com>
2020-09-16 10:29:41 +0100

    move compression code out of production block

--- server.js
+++ server.js
@@ -19,10 +19,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(cors())
-
+app.use(compression)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(compression)
   app.use(enforce.HTTPS({ trustProtoHeader: true}))
   app.use(express.static(path.join(__dirname, 'client/build')))
 
